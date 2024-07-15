4e54f51f86dcfe5ddfbe838c832f3ac062ee8e7f
Mário Branco <mario.branco18@gmail.com>
2024-07-15 09:53:38 +0100

    Temp commit

--- src/App.js
+++ src/App.js
@@ -1,36 +1,18 @@
 import React from 'react';
 import './App.css';
 import Nav from './components/Nav';
-import mainImage from './assets/random-image.png';
+import HomePage from './HomePage';
+// import FeaturedProjects from './FeaturedProjects';
 
 function App() {
+  // const organization = 'INESCTEC'; 
+  // const areaName = 'energy'; 
+
   return (
     <div className="App">
       <Nav />
-      <main className="flex flex-col md:flex-row justify-between items-center p-8 text-white">
-        <div className="text-section md:w-1/2 md:pr-8 text-left">
-          <h1 className="text-6xl md:text-8xl font-bold leading-tight ml-24">
-            <div className="flex items-baseline mb-4">
-              <span className="text-white text-4xl md:text-6xl text-white">O</span>
-              <span className="text-2xl md:text-5xl text-inesc-blue">pen</span>
-            </div>
-            <div className="flex items-baseline mb-4">
-              <span className="text-6xl md:text-6xl text-white">S</span>
-              <span className="text-4xl md:text-5xl text-inesc-blue">ource</span>
-            </div>
-            <div className="flex items-baseline mb-4">
-              <span className="text-6xl md:text-6xl text-white">S</span>
-              <span className="text-4xl md:text-5xl text-inesc-blue">oftware</span>
-            </div>
-          </h1>
-          <a href="#explore" className="inline-block mt-8 text-xl hover:underline ml-24">
-            Explore {'>'}
-          </a>
-        </div>
-        <div className="image-section md:w-1/2">
-          <img src={mainImage} alt="Open Source Software" className="w-full max-w-lg" />
-        </div>
-      </main>
+      <HomePage />
+      {/* <FeaturedProjects org={organization} areaName={areaName} /> */}
     </div>
   );
 }
