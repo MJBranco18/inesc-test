5c0f0f6a5a4955afadb62e1dd9e41456b46f33e8
Mário Branco <mario.branco18@gmail.com>
2024-08-02 01:28:33 +0100

    Deleted useless files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Nav from './components/Nav';
 import HomePage from './pages/HomePage';
 import ProjectsPage from './pages/ProjectsPage';
 
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Link, Element } from 'react-scroll';
+import { Link as ScrollLink, Element } from 'react-scroll';
+import { Link as RouterLink } from 'react-router-dom';
 import logo from '../assets/INESCTEC_logotipo_monocrom_white.png';
 import Footer from '../components/Footer'; 
 
@@ -13,40 +14,42 @@ const HomePage = () => {
             <h2 className="text-2xl md:text-3xl">Open Source Software</h2>
           </div>
           <div className="flex flex-col items-center md:items-start mt-8 md:mt-6">
-            <Link
+            <ScrollLink
               to="vision"
               smooth={true}
               duration={500}
               className="text-xl md:text-2xl hover:underline mb-6 cursor-pointer"
             >
               {'>'} Our Vision
-            </Link>
-            <Link
-              to="projects"
-              smooth={true}
-              duration={500}
+            </ScrollLink>
+            <RouterLink
+              to="/projects"
               className="text-xl md:text-2xl hover:underline cursor-pointer"
             >
               {'>'} Our Projects
-            </Link>
+            </RouterLink>
           </div>
         </div>
       </main>
       <Element name="vision" className="min-h-screen bg-white p-8">
-        <h1 className="text-4xl mb-4 gradient-text">Open Source</h1>
-        <hr className="border-0 h-1 bg-gradient-to-r from-dark-blue to-light-blue mb-8" />
-        <h2 className="text-3xl mb-4">Our Vision</h2>
-        <p className="text-xl mb-8">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas dapibus luctus enim sed semper. Aliquam pellentesque sem in gravida aliquet.
-        </p>
-        <h2 className="text-3xl mb-4">Where to start?</h2>
-        <p className="text-xl mb-8">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas dapibus luctus enim sed semper. Aliquam pellentesque sem in gravida aliquet.
-        </p>
-        <h2 className="text-3xl mb-4">Contacts</h2>
-        <p className="text-xl mb-8">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas dapibus luctus enim sed semper. Aliquam pellentesque sem in gravida aliquet.
-        </p>
+        <div className='mx-20'>
+          <h1 className="text-4xl mt-6 mb-4 gradient-text text-start">Open Source</h1>
+          <hr className="border-0 h-1 bg-gradient-to-r from-dark-blue to-light-blue mb-8" />
+          <div className="mt-20">
+            <h2 className="text-xl text-black text-start mb-4 font-bold">Our Vision</h2>
+            <p className="text-md text-black text-start mb-12">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas dapibus luctus enim sed semper. Aliquam pellentesque sem in gravida aliquet.
+            </p>
+            <h2 className="text-xl text-black text-start mb-4 font-bold">Where to start?</h2>
+            <p className="text-md text-black text-start mb-12">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas dapibus luctus enim sed semper. Aliquam pellentesque sem in gravida aliquet.
+            </p>
+            <h2 className="text-xl text-black text-start mb-4 font-bold">Contacts</h2>
+            <p className="text-md text-black text-start mb-12">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas dapibus luctus enim sed semper. Aliquam pellentesque sem in gravida aliquet.
+            </p>
+          </div>
+        </div>
       </Element>
       <Footer />
     </div>
